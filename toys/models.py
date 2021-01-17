0585ffb70c4ace0a6b013f428c4f993ccf8f0650
Matthew <kfm.yong@gmail.com>
2021-01-17 13:50:55 +0000

    Add toys model in toys app models.py

--- toys/models.py
+++ toys/models.py
@@ -7,3 +7,18 @@ class Age(models.model):
 
     def __str__(self):
         return self.name
+
+    def get_frontend_name(self):
+        return self.frontend_name
+
+
+class Toys(models.model):
+    age = models.ForeignKey('Age', null=True, blank=True, ondelete=models.SET_NULL)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
