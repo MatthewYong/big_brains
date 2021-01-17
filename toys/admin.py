759b9c163033d240aee474949acefd210f54e46f
Matthew <kfm.yong@gmail.com>
2021-01-17 14:06:12 +0000

    Renamed media files & Add site register in toys app admin.py

--- toys/admin.py
+++ toys/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Toys, Age
 
-# Register your models here.
+admin.site.register(Toys)
+admin.site.register(Age)
--- toys/models.py
+++ toys/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Age(models.model):
+class Age(models.Model):
     name = models.CharField(max_length=200)
     frontend_name = models.CharField(max_length=200, null=True, blank=True)
 
@@ -12,13 +12,13 @@ class Age(models.model):
         return self.frontend_name
 
 
-class Toys(models.model):
-    age = models.ForeignKey('Age', null=True, blank=True, ondelete=models.SET_NULL)
+class Toys(models.Model):
+    age = models.ForeignKey('Age', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    
+
     def __str__(self):
         return self.name
