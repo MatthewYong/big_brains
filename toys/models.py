ad7bfa1dcd31d558e2e6c6f80873cc95c1f1f871
Matthew <kfm.yong@gmail.com>
2021-01-17 13:14:28 +0000

    Created age model in toys models.py

--- toys/models.py
+++ toys/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Age(models.model):
+    name = models.CharField(max_length=200)
+    frontend_name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
