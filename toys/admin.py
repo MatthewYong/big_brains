e47223e04b2e4380c34cf43e713d6f62a537222d
Matthew <kfm.yong@gmail.com>
2021-01-17 14:46:05 +0000

    Add models.admin in toys app admin.py

--- toys/admin.py
+++ toys/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Toys, Age
 
+class ToysAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'age',
+        'price',
+        'image',
+    )
+
+
 admin.site.register(Toys)
 admin.site.register(Age)
