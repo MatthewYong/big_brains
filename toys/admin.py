6e41c16465c9b3f792bf556033f907130dda065d
Matthew <kfm.yong@gmail.com>
2021-01-17 14:47:36 +0000

    Add Agemodels.admin in toys app admin.py

--- toys/admin.py
+++ toys/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 from .models import Toys, Age
 
+
 class ToysAdmin(admin.ModelAdmin):
     list_display = (
-        'sku',
         'name',
         'age',
         'price',
         'image',
     )
 
+
+class AgeAdmin(admin.ModelAdmin):
+    list_display = (
+        'frontend_name',
+    )
 
 admin.site.register(Toys)
 admin.site.register(Age)
