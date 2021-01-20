472813f614fb262b00cd2f2bbaf47cc753503a60
Matthew <kfm.yong@gmail.com>
2021-01-20 21:32:16 +0000

    Add date field and changed fields to required in models.py of blogs app

--- blogs/models.py
+++ blogs/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Blog(models.Model):
 
-    title = models.CharField(max_length=200, null=True, blank=True)
-    date = models.CharField(max_length=200, null=True, blank=True)
-    author = models.CharField(max_length=30, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now=True, null=True)
+    author = models.CharField(max_length=50)
     story = models.CharField(max_length=10000)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.title
