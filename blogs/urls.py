3e8a898e4c07b6b4155f28f4c89863811ca1a659
Matthew <kfm.yong@gmail.com>
2021-01-23 15:08:59 +0000

    Add views and url pattern to blog_detail.html in blog app

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_blogs, name='blogs'),
+    path('<blog_id>', views.blog_detail, name='blog_detail'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 
 def all_blogs(request):
-    """A view to show the fields in the blog"""
+    """A view to show the fields of each blog"""
 
     blogs = Blog.objects.all()
 
@@ -13,3 +13,14 @@ def all_blogs(request):
 
     return render(request, 'blogs/blogs.html', context)
 
+
+def blog_detail(request, blog_id):
+    """A view to show the fields of the blog"""
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, 'blogs/blog_detail.html', context)
