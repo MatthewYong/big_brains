6c32037501d1fdcc62f7dc91b8539a93e46d15da
Matthew <kfm.yong@gmail.com>
2021-01-24 14:52:00 +0000

    Removed old code in nav base.html and add toys.js

---
+++ static/js/toys.js
@@ -0,0 +1,12 @@
+<script>
+    $(function(){
+    
+    $(".dropdown-menu li a").click(function(){
+        
+        $(".btn:first-child").text($(this).text());
+        $(".btn:first-child").val($(this).text());
+    });
+
+    });    
+
+</script>
