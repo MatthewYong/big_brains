71ad980b02a5b7ca357304439527e18d885efe08
Matthew <kfm.yong@gmail.com>
2021-01-27 09:27:24 +0000

    Added iteration throught cart items in context.py of cart app

--- cart/context.py
+++ cart/context.py
@@ -1,15 +1,28 @@
+from django.shortcuts import get_object_or_404
+from toys.models import Toy
 
 
 def cart_contents(request):
-
+    """Returns the cart items throughout the website through contexts"""
     cart_items = []
     total = 0
-    toys_count = 0
+    toy_count = 0
+    cart = request.session.get('cart', {})
+
+    for toy_id, quantity in cart.items():
+        toy = get_object_or_404(Toy, pk=toy_id)
+        total += quantity * toy.price
+        toy_count += quantity
+        cart_items.append({
+            'toy_id': toy_id,
+            'quantity': quantity,
+            'toy': toy,
+        })
 
     context = {
         'cart_items': cart_items,
         'total': total,
-        'toys_count': toys_count,
+        'toy_count': toy_count,
     }
 
     return context
--- cart/views.py
+++ cart/views.py
@@ -6,17 +6,17 @@ def view_cart(request):
     return render(request, 'cart/cart.html')
 
 
-def add_to_cart(request, item_id):
+def add_to_cart(request, toy_id):
     """Add a quantity of toys to the cart"""
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if toy_id in list(cart.keys()):
+        cart[toy_id] += quantity
     else:
-        cart[item_id] = quantity
+        cart[toy_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
+
     return redirect(redirect_url)
