d7994565f9697b5bef7ba6a8142b44e223dab2ce
Matthew <kfm.yong@gmail.com>
2021-01-27 21:08:44 +0000

    Fixed iteration view in cart.html

--- cart/context.py
+++ cart/context.py
@@ -6,12 +6,14 @@ def cart_contents(request):
     """Returns the cart items throughout the website through context"""
     cart_items = []
     total = 0
+    subtotal = 0
     toy_count = 0
     cart = request.session.get('cart', {})
 
     for toy_id, quantity in cart.items():
         toy = get_object_or_404(Toy, pk=toy_id)
         total += quantity * toy.price
+        subtotal += quantity * toy.price
         toy_count += quantity
         cart_items.append({
             'toy_id': toy_id,
@@ -22,6 +24,7 @@ def cart_contents(request):
     context = {
         'cart_items': cart_items,
         'total': total,
+        'subtotal': subtotal,
         'toy_count': toy_count,
     }
 
