5197c1d47536d9bee9045ba1e6580322de91308b
Matthew <kfm.yong@gmail.com>
2021-01-28 10:05:27 +0000

    Add update button and url in cart app

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add/<toy_id>/', views.add_to_cart, name='add_to_cart'),
+    path('update/<toy_id>/', views.update_cart, name='update_cart'),
     path('remove/<toy_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
