b139eaa7e0b6c716853c150e24f193f5d4420cae
Matthew <kfm.yong@gmail.com>
2021-02-01 20:55:07 +0000

    Add setup variables in stripe.js

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -1,3 +1,9 @@
-/*Code derived from Stripe website: https://stripe.com/docs/payments/accept-a-payment?integration=elements*/
+/*Code used from CI checkout lesson which is derived from Stripe website: https://stripe.com/docs/payments/accept-a-payment?integration=elements*/
 
 
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var card = elements.create('card');
+card.mount('#card-element');
