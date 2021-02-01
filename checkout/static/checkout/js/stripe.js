fdadc3578673cc7f45bed60e9a7ac34e4cb6e93d
Matthew <kfm.yong@gmail.com>
2021-02-01 21:00:08 +0000

    Add style to checkout button checkout.css and stripe.js

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -1,4 +1,6 @@
-/*Code used from CI checkout lesson which is derived from Stripe website: https://stripe.com/docs/payments/accept-a-payment?integration=elements*/
+/*
+Code is tweaked to match to the website's theme, but mainly used from CI checkout lesson which is derived from Stripe website: https://stripe.com/docs/payments/accept-a-payment?integration=elements
+*/
 
 
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
@@ -6,4 +8,22 @@ var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 var card = elements.create('card');
+card.mount('#card-element');
+
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {style: style});
 card.mount('#card-element');
