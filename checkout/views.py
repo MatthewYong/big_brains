e9fd992c469a27b14aaf869786952431ac299397
Matthew <kfm.yong@gmail.com>
2021-02-02 19:40:31 +0000

    Installed stripe and add cartcontent in views of checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -2,18 +2,25 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from .forms import OrderForm
+from cart.context import cart_contents
+
+import stripe
 
 
 def checkout(request):
     """
     A view to get the items from the cart
     and pass it into the checkout form.
-    Code used from CI checkout lesson
+    Code used from CI checkout and Stripe lessons
     """
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, 'Your cart is empty')
         return redirect(reverse('toys'))
+
+    current_cart = cart_contents(request)
+    total = current_cart['total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
