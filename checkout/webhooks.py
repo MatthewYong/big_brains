501e7df6a05f849abcf44da2bc9d0b2dff91b0c6
Matthew <kfm.yong@gmail.com>
2021-02-03 20:10:52 +0000

    Add webhook function

---
+++ checkout/webhooks.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from checkout.webhook_handler import StripeWH_Handler
+
+import stripe
+
+def webhook(request):
+    """
+    A function that listens to webhooks from Stripe
+    Code used from CI Stripe lesson
+    """
+    # Setup
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    # Retrieval of webhook data and verification of signature
+    payload = request.body
+    event = None
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+        payload, sig_header, wh_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+    except Exception as e:
+        return HttpResponse(content=e, status=400)
