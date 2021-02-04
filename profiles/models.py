865617921b4f80aabf1250809f1a2b1013692692
Matthew <kfm.yong@gmail.com>
2021-02-04 19:48:29 +0000

    Created profile class in models.py of profiles app

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    A model to let users check their order history
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_email_address = models.CharField(max_length=50, null=False, blank=False)
+
+
+
