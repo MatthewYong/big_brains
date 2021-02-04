03f6954f5babb4865cb1b45d1cb89c68421ff6ef
Matthew <kfm.yong@gmail.com>
2021-02-04 19:54:43 +0000

    Add signal to models.py in profiles app

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -7,7 +9,19 @@ class Profile(models.Model):
     A model to let users check their order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_email_address = models.CharField(max_length=50, null=False, blank=False)
+    profile_email_address = models.CharField(
+                        max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.user.username
 
 
-
+@receiver(post_save, sender=User)
+def create_or_update_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        Profile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofiel.save()
