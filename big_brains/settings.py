373961b8ca39c75d81ade293c9ac19aee75d208d
Matthew <kfm.yong@gmail.com>
2021-02-06 16:21:18 +0000

    set debug to development

--- big_brains/settings.py
+++ big_brains/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'de4hul-on8d$@=7xae!_&ewlr(%uov@qgz6!0t^n@(!+c3#mpb'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['big-brains.herokuapp.com', 'localhost']
 
