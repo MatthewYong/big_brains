86a9d90e3b613347e71c2ab8a26fe27bf05d1527
Matthew <kfm.yong@gmail.com>
2021-02-06 20:50:41 +0000

    Add cache control in settings.py

--- big_brains/settings.py
+++ big_brains/settings.py
@@ -181,6 +181,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 if 'USE_AWS' in os.environ:
+    # Place static files in cache
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # A configuration bucket from AWS
     AWS_STORAGE_BUCKET_NAME = 'matthew-big-brains'
     AWS_S3_REGION_NAME = 'eu-west-2'
