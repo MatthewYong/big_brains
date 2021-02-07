702b61b4dfa85d32c6f97b9dc19c91c20387bd0e
Matthew <kfm.yong@gmail.com>
2021-02-07 15:38:46 +0000

    Added blogform in forms.py

--- blogs/forms.py
+++ blogs/forms.py
@@ -2,3 +2,8 @@ from django import forms
 from .models import Blog
 
 
+class BlogForm(forms.ModelForm):
+    class Meta:
+        article = Blog
+        fields = ('title', 'description', 'date',
+                  'author', 'story', 'image-url')
