3c0648b15ab6ced9ab5473ceadc64b13db85ca2a
Matthew <kfm.yong@gmail.com>
2021-02-07 21:23:25 +0000

    Simplified blog urls in blog app

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.all_blogs, name='blogs'),
     path('<blog_id>', views.blog_detail, name='blog_detail'),
-    path('blogs/blog_add/', views.blog_add, name='blog_add'),
+    path('blog_add/', views.blog_add, name='blog_add'),
     path('blogs/<blog_id>', views.blog_delete, name='blog_delete'),
 ]
