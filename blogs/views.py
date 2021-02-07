a684551a3ea80e18f4b45e31ec4bc0be3fd9b5f0
Matthew <kfm.yong@gmail.com>
2021-02-07 16:44:12 +0000

    Add blog_add view in blog views.py

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+
 from .models import Blog
+from .forms import BlogForm
 
 
 def all_blogs(request):
@@ -15,7 +17,9 @@ def all_blogs(request):
 
 
 def blog_detail(request, blog_id):
-    """A view to show the fields of the blog"""
+    """
+    A view to show the fields of the blog
+    """
 
     blog = get_object_or_404(Blog, pk=blog_id)
 
@@ -24,3 +28,19 @@ def blog_detail(request, blog_id):
     }
 
     return render(request, 'blogs/blog_detail.html', context)
+
+
+def blog_add(request):
+    """
+    A view to add new blogs through a user profile
+    """
+
+    blog_form = BlogForm()
+
+    template = 'checkout/checkout.html'
+    context = {
+        'blog_form': blog_form,
+    }
+
+    return render(request, template, context)
+
