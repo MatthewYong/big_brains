469ded638f282597380bed370ca6f12a10802e77
Matthew <kfm.yong@gmail.com>
2021-02-09 09:16:16 +0000

    Add review model in review app

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False)
+    comment = models.CharField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
