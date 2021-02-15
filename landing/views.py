0572205b228c67d483733bb73782f32c30ef31c8
Matthew <kfm.yong@gmail.com>
2021-02-15 21:02:10 +0000

    Added send_mail functionality in views.py of landing app

--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def index(request):
-    """A view that shows the index page"""
+    """
+    A view that shows the landing page and sends
+    an email to the user
+    """
+
+    if request.method == 'POST':
+        message = request.POST['message']
+        send_mail('Contact Form',
+                  message,
+                  settings.EMAIL_HOST_USER,
+                  ['kfm.yong@gmail.com'],
+                  fail_silently=False)
+
     return render(request, 'landing/index.html')
