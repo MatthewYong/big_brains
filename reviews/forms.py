869ff7828288b78b0fe18c325d915c90903e4565
Matthew <kfm.yong@gmail.com>
2021-02-19 09:24:48 +0000

    Add widget in form of reviews app

--- reviews/forms.py
+++ reviews/forms.py
@@ -10,3 +10,15 @@ class ToyReviewForm(forms.ModelForm):
             'name',
             'comment',
         )
+
+        # Widgets to add place holders and control layout of the form. Code
+        # partially used from: https://www.youtube.com/watch?v=VYs-u0g
+        # 1A&t=20s&ab_channel=PrettyPrinted
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'input',
+                                           'placeholder': 'Your name'}),
+            'comment': forms.Textarea(attrs={'class': 'textarea', 'rows': 5,
+                                             'placeholder':
+                                             'Write your review...'}),
+        }
