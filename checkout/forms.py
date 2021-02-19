910cb5d5ef5290595ee8a2d1a3b756da5dd5dfcc
Matthew <kfm.yong@gmail.com>
2021-02-19 11:44:28 +0000

    Edit checkout form style

--- checkout/forms.py
+++ checkout/forms.py
@@ -8,29 +8,21 @@ class OrderForm(forms.ModelForm):
         fields = ('first_name', 'last_name', 'email_address', 'address',
                   'postcode', 'town', 'country', 'comments')
 
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'input',
+                'placeholder': 'First Name'}),
+            'comments': forms.Textarea(attrs={
+                'class': 'textarea', 'rows': 5,
+                'placeholder': 'Comments'}),
+        }
+
     def __init__(self, *args, **kwargs):
         """
         Function to add placeholders and classes,
         code used from CI checkout lesson
         """
         super().__init__(*args, **kwargs)
-        placeholders = {
-            'first_name': 'First Name',
-            'last_name': 'Last Name',
-            'email_address': 'Email Address',
-            'address': 'Address',
-            'postcode': 'Postcode',
-            'town': 'Town',
-            'comments': 'Add your comments',
-        }
 
-        self.fields['first_name'].widget.attrs['autofocus'] = False
         for field in self.fields:
-            if field != 'country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
