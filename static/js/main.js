3100e3dd4b161a5faf56524d256798f7b34a9aab
Matthew <kfm.yong@gmail.com>
2021-02-24 20:00:07 +0000

    Add fade animation on landing page

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,19 @@
-/*Code inspired from https://www.w3schools.com/howto/howto_js_scroll_to_top*/
-function scrollWintop() {
-    window.scrollTo(0,0);
-}
+$("document").ready(function () {
+    AOS.init();
+    
+    $('.toast').toast('show');
+    
+    /*
+    $(".image-product-1").on("click", function () {
+        $(this).addClass("testjs");
+    });    */
+
+
+    /*On click scroll to top*/
+    $('#scroll-top-button').on("click", function() {
+        window.scrollTo(0,0)
+    })
+});
+
+
+
