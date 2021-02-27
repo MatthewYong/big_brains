e69d7adb2027b8cd0ffde6714af4b45e43e560f1
Matthew <kfm.yong@gmail.com>
2021-02-27 14:37:36 +0000

    Add test case for blogform

--- blogs/tests.py
+++ blogs/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .forms import BlogForm
 
-# Create your tests here.
+
+class TestBlogForm(TestCase):
+
+    def test_field_is_required(self):
+        """
+        A test case to prove that the form is working
+        """
+        form = BlogForm({'title': ''})
+        self.assertTrue(form.is_valid)
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
--- toys/tests.py
+++ toys/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+"""
+class TestDjango(TestCase):
+
+    def test_toys(self):
+        self.assertEqual(1,0)
+
+    def test_toys2(self):
+        self.assertEqual(0,0)
+"""
